5c38e03182a0f16965cf46ce29b93558a2a2370c
Jill Marcum <jill.marcum@gmail.com>
2020-03-13 21:21:42 +0000

    updated database connection

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host: '127.0.0.1',
+    host: 'postgresql-transparent-26620',
     user: 'postgres',
     password: '',
     database: 'smartbrain'
